7eaae0abd44620f1a7f8cd3e19d8a6cd18e5621c
busami-star <victorbusami1@gmail.com>
2025-01-15 23:54:16 +0300

    first commit

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,28 @@
+
+import {assets} from "../assets/assets.js";
+
+const Header = () => {
+    return (
+        <div>
+            <div>
+<p>Book Appointment <br/> With Trusted Doctors</p>
+                <div>
+                    <img src={assets.group_profiles} alt="" />
+                    <p>simply browse through our extensive list of trusted doctors,<br/> schedule your appointments seemlessly</p>
+
+            </div>
+            <a href=''>
+                Book appointment <img src={assets.arrow_icon} alt='' />
+            </a>
+        </div>
+
+
+
+
+            <div>
+
+            </div>
+        </div>
+    )
+}
+export default Header
